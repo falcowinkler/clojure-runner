0975e5bd90695f606a9e7a543ad0947cf03f66b5
Falco Winkler <8613031+falcowinkler@users.noreply.github.com>
2024-08-01 17:17:51 +0200

    add slashes

--- bin/run.sh
+++ bin/run.sh
@@ -13,8 +13,8 @@ W=/workspace
 
 C=$($CONTAINER_ENGINE container create --rm $IMAGE_TAG sh -c "lein run -m main")
 
-$CONTAINER_ENGINE container cp examples/${1:-multiply}/solution.clj $C:$W/src/codewars
-$CONTAINER_ENGINE container cp examples/${1:-multiply}/solution_test.clj $C:$W/test/codewars
+$CONTAINER_ENGINE container cp examples/${1:-multiply}/solution.clj $C:$W/src/codewars/
+$CONTAINER_ENGINE container cp examples/${1:-multiply}/solution_test.clj $C:$W/test/codewars/
 
 # Run tests
 $CONTAINER_ENGINE container start --attach $C
